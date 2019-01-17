f8b168183c77fb034652b56ee2d9e1b3a0275938
nmarks413 <nmarks413@gmail.com>
2019-01-16 16:21:15 -0800

    added setValue functions for double solenois \n Changed solenoids to DoubleSolenoids

--- src/main/java/frc/robot/subsystems/HatchIntake.java
+++ src/main/java/frc/robot/subsystems/HatchIntake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -12,13 +13,33 @@ public class HatchIntake extends Subsystem{
         return instance;
     }
 
-    public Solenoid solenoid1;
-    public Solenoid solenoid2;
+    public DoubleSolenoid solenoid1;
+    public DoubleSolenoid solenoid2;
 
     private HatchIntake(){
-        solenoid1 = new Solenoid(0);
-        solenoid2 = new Solenoid(1);
+        solenoid1 = new DoubleSolenoid(0,1);
+        solenoid2 = new DoubleSolenoid(2,3);
     }
+    public void outSolenoid1(){
+        solenoid1.set(DoubleSolenoid.Value.kForward);
+
+    }
+    public void inSolenoid1(){
+        solenoid1.set(DoubleSolenoid.Value.kReverse);
+
+    }
+
+    public void outSolenoid2(){
+        solenoid2.set(DoubleSolenoid.Value.kForward);
+
+    }
+
+    public void inSolenoid2(){
+        solenoid2.set(DoubleSolenoid.Value.kReverse);
+
+    }
+
+
     @Override
     protected void initDefaultCommand() {
 
