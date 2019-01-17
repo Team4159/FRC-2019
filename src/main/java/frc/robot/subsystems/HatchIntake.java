70a36b8a8cc9ba1875f85b85862dfee622f6085b
nmarks413 <nmarks413@gmail.com>
2019-01-16 16:32:44 -0800

    using Constants class

--- src/main/java/frc/robot/subsystems/HatchIntake.java
+++ src/main/java/frc/robot/subsystems/HatchIntake.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.util.Constants;
 
 public class HatchIntake extends Subsystem{
     private static HatchIntake instance;
@@ -15,10 +16,12 @@ public class HatchIntake extends Subsystem{
 
     public DoubleSolenoid solenoid1;
     public DoubleSolenoid solenoid2;
+    public Constants constants;
 
     private HatchIntake(){
-        solenoid1 = new DoubleSolenoid(0,1);
-        solenoid2 = new DoubleSolenoid(2,3);
+        constants = Constants.getInstance();
+        solenoid1 = new DoubleSolenoid(constants.getInt("Solenoid1Channell"),constants.getInt("Solenoid1Channel2"));
+        solenoid2 = new DoubleSolenoid(constants.getInt("Solenoid2Channel1"),constants.getInt("Solenoid2Channel2"));
     }
     public void outSolenoid1(){
         solenoid1.set(DoubleSolenoid.Value.kForward);
