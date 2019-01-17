c02f0a123c32302dfdac4587433677c3c46562b1
Brandon Lou <brandonlou@users.noreply.github.com>
2019-01-17 15:42:35 -0800

    Add Drivetrain and Elevator Template (#1)
    * Add template
    
    * Add drivetrain template
    
    * Add drivetrain and elevator template
    
    * Upgrade WPILib to 2.1

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import frc.robot.util.Constants;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+
+    private static OI instance;
+
+    public static OI getInstance() {
+        if(instance == null)
+            instance = new OI();
+        return instance;
+    }
+
+    private Joystick leftJoy, rightJoy, xbox;
+    private Constants constants;
+
+    private OI() {
+        leftJoy = new Joystick(constants.getInt("LEFT_JOY"));
+        rightJoy = new Joystick(constants.getInt("RIGHT_JOY"));
+        xbox = new Joystick(constants.getInt("XBOX"));
+    }
+
+    public double getLeftY() {
+        return leftJoy.getY();
+    }
+
+    public double getRightY() {
+        return rightJoy.getY();
+    }
+
+}
