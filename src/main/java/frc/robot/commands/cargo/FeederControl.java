6f090ff835756da7bdc43e5607fb6c87e826fc7e
Rishi Nath <rishi.nath@gmail.com>
2019-01-24 14:56:02 -0800

    command

--- src/main/java/frc/robot/commands/cargo/FeederControl.java
+++ src/main/java/frc/robot/commands/cargo/FeederControl.java
@@ -1,4 +1,50 @@
 package frc.robot.commands.cargo;
 
-public class FeederControl {
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.OI;
+import frc.robot.subsystems.Feeder;
+
+public class FeederControl extends Command {
+
+    private Feeder feeder;
+    private OI oi;
+
+    public FeederControl() {
+
+        feeder = Feeder.getInstance();
+        oi = OI.getInstance();
+
+        requires(feeder);
+    }
+
+    @Override
+    protected void execute() {
+        if (oi.getFeederIntake()) {
+            feeder.intake();
+        } else
+        if (oi.getFeederOuttake()) {
+            feeder.outtake();
+        } else {
+            feeder.stop();
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+
+        return false;
+
+    }
+
+    @Override
+    protected void end() {
+
+        feeder.stop();
+
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
+    }
 }
