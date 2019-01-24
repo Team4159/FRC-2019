4d4046b7e15bc431169a5907090c901999dafe5d
Rishi Nath <rishi.nath@gmail.com>
2019-01-24 13:32:49 -0800

    subsystem

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,4 +1,39 @@
 package frc.robot.subsystems;
 
-public class Feeder {
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.util.Constants;
+
+public class Feeder extends Subsystem {
+
+    private VictorSPX feederVictor;
+    private static Feeder instance,
+
+    public static Feeder getInstance(){
+        if (instance == null) {
+            instance = new Feeder();
+        }
+        return instance;
+    }
+
+    private Feeder() {
+        //Need to add to constants
+        feederVictor = new VictorSPX(Constants.getInt("FEEDER_VICTOR"));
+    }
+
+    public void stop() { feederVictor.set(ControlMode.PercentOutput, 0); }
+
+    public void intake() { feederVictor.set(ControlMode.PercentOutput, 1); }
+
+    public void outtake() { feederVictor.set(ControlMode.PercentOutput, -1); }
+
+    @Override
+    protected void initDefaultCommand() {
+        setDefaultCommand(new FeederControl());
+    }
+
+
 }
+
