24999e8c0aa22e4534f00b5b23f583b93a795b7f
Kai Chang <kaijchang@gmail.com>
2019-01-31 20:15:53 -0800

    config open loop ramping

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,9 +1,13 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.commands.drive.DriveControl;
 import frc.robot.util.Constants;
 
@@ -31,6 +35,14 @@ public class Drivetrain extends Subsystem {
         rightMasterTalon.configFactoryDefault();
         rightSlaveTalon.configFactoryDefault();
 
+        rightMasterTalon.setInverted(true);
+        rightSlaveTalon.setInverted(true);
+
+        leftMasterTalon.enableVoltageCompensation(true);
+        rightMasterTalon.enableVoltageCompensation(true);
+        leftSlaveTalon.enableVoltageCompensation(true);
+        rightSlaveTalon.enableVoltageCompensation(true);
+
         leftSlaveTalon.follow(leftMasterTalon);
         rightSlaveTalon.follow(rightMasterTalon);
 
@@ -38,7 +50,6 @@ public class Drivetrain extends Subsystem {
     }
 
     public void rawDrive(double left, double right) {
-
         leftMasterTalon.set(ControlMode.PercentOutput, left);
         rightMasterTalon.set(ControlMode.PercentOutput, right);
 
@@ -60,6 +71,42 @@ public class Drivetrain extends Subsystem {
     }
 
     public void setPathFollowingConfig() {
+
+    }
+
+    public void setBrownoutPreventionConfig() {
+
+        leftMasterTalon.configOpenloopRamp(leftMasterTalon.getBusVoltage() / 0.25);
+        rightMasterTalon.configOpenloopRamp(rightMasterTalon.getBusVoltage() / 0.25);
+
+    }
+
+    /**
+     * @return Left side drivetrain velocity in feet/sec
+     */
+    private double getLeftVelocity() {
+
+        return leftMasterTalon.getSelectedSensorVelocity() * 4.60194236366; // assuming 1 rev = 4096 ticks
+
+    }
+
+    /**
+     * @return Right side drivetrain velocity in feet/sec
+     */
+    private double getRightVelocity() {
+
+        return rightMasterTalon.getSelectedSensorVelocity() * 4.60194236366; // assuming 1 rev = 4096 ticks
+
+    }
+
+    private ShuffleboardTab tab = Shuffleboard.getTab("Drive");
+    private NetworkTableEntry leftEntry = tab.add("Left Drivetrain Velocity", 0).withWidget(BuiltInWidgets.kGraph).getEntry();
+    private NetworkTableEntry rightEntry = tab.add("Right Drivetrain Velocity", 0).withWidget(BuiltInWidgets.kGraph).getEntry();
+
+    public void logDashboard() {
+
+        leftEntry.setDouble(getLeftVelocity());
+        rightEntry.setDouble(getRightVelocity());
 
     }
 
