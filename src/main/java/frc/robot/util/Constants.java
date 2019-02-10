9f6856199b8011b0d9669624b9dc379eee5b7076
brandonlou <brandonlou08@gmail.com>
2019-02-09 18:28:53 -0800

    Instantiate subsystems, fix constants, styling

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -1,7 +1,6 @@
 package frc.robot.util;
 
 import org.jetbrains.annotations.NotNull;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
--- src/main/java/frc/robot/util/RobotLogger.java
+++ src/main/java/frc/robot/util/RobotLogger.java
@@ -2,11 +2,9 @@ package frc.robot.util;
 
 import java.io.File;
 import java.io.IOException;
-
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.function.Supplier;
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.Handler;
@@ -14,12 +12,13 @@ import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 import java.util.stream.Collectors;
 import java.util.logging.FileHandler;
-
 import edu.wpi.first.wpilibj.Notifier;
 import frc.robot.subsystems.Superstructure;
 
 public class RobotLogger implements Runnable {
+
     private class LogFormatter extends Formatter {
+
         @Override
         public String getHead(Handler h) {
             return String.join(",", fields.keySet());
@@ -29,14 +28,13 @@ public class RobotLogger implements Runnable {
         public String format(LogRecord record) {
             return record.getMessage();
         }
+
     }
 
     private static RobotLogger instance;
-
     public static RobotLogger getInstance() {
         if (instance == null)
             instance = new RobotLogger();
-
         return instance;
     }
 
@@ -48,12 +46,13 @@ public class RobotLogger implements Runnable {
     private HashMap<String, Supplier<Object>> fields;
 
     private RobotLogger() {
+
         notifier = new Notifier(this);
         notifier.startPeriodic(0.2);
 
         superstructure = Superstructure.getInstance();
 
-        fields = new HashMap<String, Supplier<Object>>() {
+        fields = new HashMap<>() {
             {
                 put("MODE", superstructure::getMode);
                 put("ALLIANCE", superstructure::getAlliance);
@@ -64,13 +63,17 @@ public class RobotLogger implements Runnable {
 
         try {
             initLogger();
+
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Problems with creating the log files, check if the USB is plugged in.");
+
         }
+
     }
 
     private void initLogger() throws IOException {
+
         String drive = locateDrive();
 
         if (drive != null) {
@@ -82,21 +85,26 @@ public class RobotLogger implements Runnable {
 
             logger.addHandler(logFileHandler);
             logger.setLevel(Level.INFO);
+
         } else {
             throw new IOException("Unable to locate USB drive.");
+
         }
+
     }
 
     @Override
     public void run() {
-        logger.info(String.join(",", fields.values()
+
+        logger.info(fields.values()
                 .stream()
                 .map(handler -> handler.get().toString())
-                .collect(Collectors.toList())
-        ));
+                .collect(Collectors.joining(",")));
+
     }
 
     private String locateDrive() {
+
         String[] directories = new File("/media").list(
                 (File dir, String name) -> new File(dir, name).isDirectory()
         );
@@ -117,5 +125,6 @@ public class RobotLogger implements Runnable {
         }
 
         return null;
+
     }
 }
