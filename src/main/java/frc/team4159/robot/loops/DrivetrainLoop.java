0853eeb544bf622ca6811a25abef77b880ec568c
Rishi Nath <rishi.nath@gmail.com>
2019-09-11 13:17:28 -0700

    math

--- src/main/java/frc/team4159/robot/loops/DrivetrainLoop.java
+++ src/main/java/frc/team4159/robot/loops/DrivetrainLoop.java
@@ -1,5 +1,40 @@
 package frc.team4159.robot.loops;
 
+import frc.team4159.robot.subsystems.Drivetrain;
+
 public class DrivetrainLoop {
+    private enum State {
+        IDLE(0),
+        RUNNING(1),
+        ESTOP(2);
+
+        int value;
+        State(int value) {
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    }
+
+    State state;
+
+    private DrivetrainLoop() {
+        state = State.IDLE;
+    }
+
+    public double update(double encoder, boolean limitTriggered, boolean enabled) {
+        switch (state) {
+            case IDLE:
+                if (enabled) state = State.RUNNING;
+                break;
+            case RUNNING:
+                break;
+            case ESTOP:
+                break;
+        }
+
+        return 0.0;
+    }
 
 }
--- src/test/java/DrivetrainTest.java
+++ src/test/java/DrivetrainTest.java
@@ -1,4 +1,52 @@
+import frc.team4159.robot.Utils;
+import frc.team4159.robot.loops.DrivetrainLoop;
 import org.junit.*;
 
+/**
+ * The Drivetrain uses RevRobotic's NEO Brushless Motors.
+ *
+ * Datasheet: http://http://www.revrobotics.com/content/docs/REV-21-1650-DS.pdf
+ */
+
+
 public class DrivetrainTest {
+    // number of motors
+    private final int kMotors = 4;
+    // resistance of the motor in ohms
+    private final double kResistance = 12.0 / 105.0;
+    // motor velocity constant in RPM per volt
+    private final double Kv = Utils.RPMtoRadiansPerSec(5676.0) / (12.0 - 1.8 * kResistance);
+    // motor torque constant in torque per amp
+    private final double Kt = 2.6 / 105.0;
+    // gear ratio
+    private final double kG = 7.36;
+    // gear radius in meters
+    private final double kr = Utils.FeettoMeters(1.432 / 12.0);
+    // mass of load in kgs
+    private final double kMass = Utils.PoundstoKgs(121.4);
+    // radius of wheels in meters
+    private final double kWheelRadius = 31293021.07;
+
+    // position in m
+    private double position = 0.0;
+    // starting position in m
+    private double starting_position = 0.0;
+    // velocity in m/s
+    private double angular_velocity = 0.0;
+
+    private DrivetrainLoop drivetrain_loop;
+
+    private double getAngularAcceleration(double voltage) {
+        return (voltage - (kG * angular_velocity) / Kv) * (kG * Kt) / (kResistance * kMass);
+    }
+
+    private void simulateTime(double voltage, double time) {
+        double kSimTime = 0.0001;
+        while (time > 0) {
+            double angular_acceleration = getAngularAcceleration(voltage) * kMotors;
+            angular_velocity += angular_acceleration * kSimTime;
+            position += ((angular_velocity * kSimTime) / (2 * Math.PI)) * (2 * Math.PI * kWheelRadius);
+            time -= kSimTime;
+        }
+    }
 }
