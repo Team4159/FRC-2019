d9938cf8d48348c9f60e1cdc408488bf2ab01ee4
Rishi Nath <rishi.nath@gmail.com>
2019-10-12 18:52:32 -0700

    motionmagic feeder

--- src/main/java/frc/team4159/robot/subsystems/Feeder.java
+++ src/main/java/frc/team4159/robot/subsystems/Feeder.java
@@ -1,5 +1,7 @@
 package frc.team4159.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -8,7 +10,6 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import frc.team4159.robot.Constants;
 import frc.team4159.robot.OI;
-import frc.team4159.robot.loops.LifterLoopBad;
 
 public class Feeder implements Subsystem {
     private static Feeder instance;
@@ -19,16 +20,19 @@ public class Feeder implements Subsystem {
         return instance;
     }
 
-    private LifterLoopBad lifter_loop;
     private DriverStation ds;
 
     private OI oi;
     private TalonSRX lifter_talon, intake_talon;
+    private DigitalInput limit_switch;
+    private int goal;
+    private boolean zeroing;
 
     private Feeder() {
-        lifter_loop = new LifterLoopBad();
         ds = DriverStation.getInstance();
         oi = OI.getInstance();
+
+        limit_switch = new DigitalInput(Constants.LIFTER_LIMIT_SWITCH);
 
         intake_talon = new TalonSRX(Constants.INTAKE_TALON);
         lifter_talon = new TalonSRX(Constants.LIFTER_TALON);
@@ -38,6 +42,16 @@ public class Feeder implements Subsystem {
 
         intake_talon.setNeutralMode(NeutralMode.Brake);
         lifter_talon.setNeutralMode(NeutralMode.Brake);
+
+        lifter_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+        lifter_talon.config_kP(0, 1.0);
+        lifter_talon.config_kI(0, 0.0);
+        lifter_talon.config_kD(0, 20.0);
+        lifter_talon.config_kF(0, 0.3);
+        lifter_talon.configNeutralDeadband(0);
+
+        lifter_talon.configMotionCruiseVelocity(5000);
+        lifter_talon.configMotionAcceleration(2000);
     }
 
     @Override
@@ -49,13 +63,34 @@ public class Feeder implements Subsystem {
         }
 
         if (oi.getSecondaryJoy().getRawButton(9)) {
-            lifter_loop.setGoal(LifterLoopBad.Position.DOWN);
+            this.setGoal(0);
         } else if (oi.getSecondaryJoy().getRawButton(6)) {
-            lifter_loop.setGoal(LifterLoopBad.Position.UP);
+            // TODO: Measure
+            this.setGoal(3072);
         }
 
-        lifter_talon.set(ControlMode.PercentOutput,
-                         lifter_loop.update(ds.isEnabled()) / 12.0);
+        if (zeroed()) {
+            lifter_talon.set(ControlMode.MotionMagic, goal);
+        } else {
+            if (zeroed()) {
+                zero();
+            } else {
+                // TODO: Check which way is negative
+                lifter_talon.set(ControlMode.PercentOutput, -0.3);
+            }
+        }
+    }
+
+    private void setGoal(int goal) {
+        this.goal = goal;
+    }
+
+    private boolean zeroed() {
+        return !limit_switch.get();
+    }
+
+    private void zero() {
+        lifter_talon.setSelectedSensorPosition(0);
     }
 
     private void intake() {
